5b8af7bdf95e49e01a69a275b9c8255840dc2074
monxchrome <inacheat@gmail.com>
2023-06-12 23:50:11 +0200

    12.06.2023 Order details
    Added order details page for mobile

--- src/components/Orders/OrderDetails.tsx
+++ src/components/Orders/OrderDetails.tsx
@@ -1,9 +1,91 @@
 import React from 'react';
+import {Input, Spacer, Text} from "@nextui-org/react";
+import css from './styles/order-details.module.css'
+import CakeTwoToneIcon from '@mui/icons-material/CakeTwoTone';
+import LocalLibraryTwoToneIcon from '@mui/icons-material/LocalLibraryTwoTone';
+import LightbulbTwoToneIcon from '@mui/icons-material/LightbulbTwoTone';
+import PaidTwoToneIcon from '@mui/icons-material/PaidTwoTone';
+import LocalPhoneTwoToneIcon from '@mui/icons-material/LocalPhoneTwoTone';
+import FunctionsTwoToneIcon from '@mui/icons-material/FunctionsTwoTone';
+import AdminPanelSettingsTwoToneIcon from '@mui/icons-material/AdminPanelSettingsTwoTone';
 
-const OrderDetails = () => {
+const OrderDetails = ({order}:{order: any}) => {
+    const avatar = 'https://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png'
+
+    const {
+        name,
+        surname,
+        email,
+        phone,
+        age,
+        course,
+        course_format,
+        status,
+        sum,
+        alreadyPaid,
+        manager,
+        comments
+    } = order
+
     return (
-        <div>
-
+        <div className={css.Father}>
+            <div className={css.Mother}>
+                <div className={css.ImgDiv}>
+                    <img src={avatar} alt="" className={css.Img}/>
+                </div>
+                <div className={css.FIO}>
+                    <div>
+                        <Text h3>{name} {surname}</Text>
+                    </div>
+                    <div>
+                        <Text>{email == null ? 'null': email}</Text>
+                    </div>
+                </div>
+            </div>
+            <Spacer/>
+            <hr/>
+            <div className={css.DescDiv}>
+                <div className={css.Wrap}>
+                    <CakeTwoToneIcon sx={{fontSize: 40}}/>
+                    <Text className={css.Text} h4>Age:</Text>
+                    <Text className={css.Text} h4>{age == null ? 'null': age}</Text>
+                </div>
+                <div className={css.Wrap}>
+                    <LocalLibraryTwoToneIcon sx={{fontSize: 40}}/>
+                    <Text className={css.Text} h4>Course Format:</Text>
+                    <Text className={css.Text} h4>{course_format == null ? 'null': course_format}</Text>
+                </div>
+                <div className={css.Wrap}>
+                    <LightbulbTwoToneIcon sx={{fontSize: 40}}/>
+                    <Text className={css.Text} h4>Course:</Text>
+                    <Text className={css.Text} h4>{course == null ? 'null': course}</Text>
+                </div>
+                <div className={css.Wrap}>
+                    <PaidTwoToneIcon sx={{fontSize: 40}}/>
+                    <Text className={css.Text} h4>Paid:</Text>
+                    <Text className={css.Text} h4>{alreadyPaid == null ? 'null': alreadyPaid}</Text>
+                </div>
+                <div className={css.BottomWrap}>
+                    <LocalPhoneTwoToneIcon sx={{fontSize: 26}}/>
+                    <Text className={css.Text} h6>{phone == null ? 'null': phone}</Text>
+                </div>
+                <div className={css.BottomWrap}>
+                    <FunctionsTwoToneIcon sx={{fontSize: 26}}/>
+                    <Text className={css.Text} h6>{sum == null ? 'null': sum}</Text>
+                </div>
+                <div className={css.BottomWrap}>
+                    <AdminPanelSettingsTwoToneIcon sx={{fontSize: 26}}/>
+                    <Text className={css.Text} h6>{manager == null ? 'null': manager}</Text>
+                </div>
+            </div>
+            <div>
+                <div className={css.TextDiv}>
+                    <Text h3>Add a comment</Text>
+                </div>
+                <div className={css.InputDiv}>
+                    <Input placeholder="Comment me..." width="70vw"/>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Orders/OrdersDetails.tsx
+++ src/components/Orders/OrdersDetails.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {orderService} from "../../services/order.service";
+import OrderDetails from "./OrderDetails";
 
 const OrdersDetails = ({orderId}: any) => {
     const [order, setOrder] = useState([]);
@@ -10,7 +11,7 @@ const OrdersDetails = ({orderId}: any) => {
 
     return (
         <div>
-
+            {order.map(item => <OrderDetails key={item._id} order={item}/>)}
         </div>
     );
 };
